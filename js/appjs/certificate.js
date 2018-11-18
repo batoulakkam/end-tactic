5d7cf0346d10cc827c83208feb0b3d403fa638af
HejaziO <43334841+HejaziO@users.noreply.github.com>
2018-11-18 19:29:33 +0300

    Add files via upload

--- js/appjs/certificate.js
+++ js/appjs/certificate.js
@@ -1,5 +1,4 @@
  $(document).ready(function() {
-
      //delete event
      $(".adelete").click(function() {
          $("#hdCertificateId").val($(this).data("id"));
@@ -38,6 +37,129 @@
          });
      });
 
+     $("#lblEventName").draggable({
+         refreshPositions: true
+     });
+
+     $("#lblVisitorName").draggable({
+         refreshPositions: true
+     });
+
+     $("#lblEventDate").draggable({
+         refreshPositions: true
+     });
+
+
+
+
+
+
+     // send the info of image to write it on the image 
+     $('#passImageInfo').click(function() {
+         // get value of required variable and pass it to imagetext.php
+         debugger;
+         var eventId = $("#eventId").val();
+         var color = $("#color").val();
+         var fontSize = $("#fontSize").val();
+         var eventName = $("#lblEventName").offset();
+         var visitorName = $("#lblVisitorName").offset();
+         var eventDate = $("#lblEventDate").offset();
+         var myImg = $("#myImg").offset();
+         var lblEventNameVal = $("#lblEventName").text();
+         var lblVisitorNameVal = $("#lblVisitorName").text();
+         var lblEventDateVal = $("#lblEventDate").text();
+         eventName = "X" + (eventName.left - (myImg.left)) + "Y" + ((eventName.top) - (myImg.top));
+         visitorName = "X" + (visitorName.left - (myImg.left)) + "Y" + ((visitorName.top) - (myImg.top));
+         eventDate = "X" + (eventDate.left - (myImg.left)) + "Y" + ((eventDate.top) - (myImg.top));
+
+         var name = $("#fileToUpload")[0].files[0] == undefined ? "certificate.jpg" : $("#fileToUpload")[0].files[0].name;
+         var attendeeID = 0;
+
+         var fd = new FormData();
+         var files = $('#fileToUpload')[0].files[0];
+         fd.append('file', files);
+         fd.append('lblEventNameVal', lblEventNameVal);
+         fd.append('lblVisitorNameVal', lblVisitorNameVal);
+         fd.append('lblEventDateVal', lblEventDateVal);
+         fd.append('color', color);
+         fd.append('fontSize', fontSize);
+         fd.append('source', name);
+         fd.append('eventName', eventName);
+         fd.append('visitorName', visitorName);
+         fd.append('eventDate', eventDate);
+         fd.append('eventId', eventId);
+         fd.append('attendeeID', attendeeID);
+
+         $.ajax({
+             type: "POST",
+             url: "imageWritingCertificate.php",
+             data: fd,
+             contentType: false,
+             processData: false,
+             //{
+             //          fd
+             //     //  lblEventNameVal: lblEventNameVal,
+             //     //  lblVisitorNameVal: lblVisitorNameVal,
+             //     //  lblEventDateVal: lblEventDateVal,
+             //     //  color: color,
+             //     //  fontSize: fontSize,
+             //     //  source: name,
+             //     //  eventName: eventName,
+             //     //  visitorName: visitorName,
+             //     //  eventDate: eventDate,
+             //     //  eventId: eventId,
+             //     //  attendeeID: attendeeID
+             //  },
+             //success enter data  
+             success: function(data) {
+                 data = data + "?" + new Date().getTime();
+                 $('#viewCertificate').attr('src', data);
+                 $('#viewAttendeeCertificate').modal('show');
+             }
+
+         });
+
+     });
+
+     // this part for print image 
+     document.getElementById("btnPrintCertificate").onclick = function() {
+         printElement(document.getElementById("printCertificate"));
+     }
+
+     function printElement(elem) {
+         var domClone = elem.cloneNode(true);
+
+         var $printSection = document.getElementById("printSection");
+
+         if (!$printSection) {
+             var $printSection = document.createElement("div");
+             $printSection.id = "printSection";
+             document.body.appendChild($printSection);
+         }
+
+         $printSection.innerHTML = "";
+         $printSection.appendChild(domClone);
+         window.print();
+     }
+
+     $('#add').click(function() {
+         // get value of required variable and pass it to imageWritingCertificate.php
+         var eventName = $("#lblEventName").offset();
+         var visitorName = $("#lblVisitorName").offset();
+         var eventDate = $("#lblEventDate").offset();
+         var myImg = $("#myImg").offset();
+         eventName = ("X" + eventName.left - (myImg.left)) + "Y" + ((eventName.top) - (myImg.top));
+         visitorName = ("X" + visitorName.left - (myImg.left)) + "Y" + ((visitorName.top) - (myImg.top));
+         eventDate = ("X" + eventDate.left - (myImg.left)) + "Y" + ((eventDate.top) - (myImg.top));
+         myImg = ("X" + (myImg.left) + "Y" + (myImg.top));
+         document.getElementById('eventNamePosition').value = eventName;
+         document.getElementById('visitorNamePosition').value = visitorName;
+         document.getElementById('eventDatePosition').value = eventDate;
+         document.getElementById('imagePosition').value = myImg;
+     });
+
+
+
      $(".formDivAddCertificate").validate({
          // Specify validation rules
          rules: {
@@ -49,6 +171,15 @@
              fileToUpload: {
                  required: true,
 
+             },
+             valueposition: {
+                 required: true
+             },
+             color: {
+                 required: true
+             },
+             fontSize: {
+                 required: true
              },
 
 
@@ -63,6 +194,17 @@
 
              fileToUpload: {
                  required: "حقل مطلوب",
+             },
+             valueposition: {
+                 required: "حقل مطلوب"
+             },
+
+             color: {
+                 required: "حقل مطلوب"
+             },
+
+             fontSize: {
+                 required: "حقل مطلوب"
              },
 
          }
