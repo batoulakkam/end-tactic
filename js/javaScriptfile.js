0978d05f9234005bb281050c51a1d0f3323325c4
marwaSalahi <36669716+marwaSalahi@users.noreply.github.com>
2018-11-21 12:03:22 +0300

    Add files via upload

--- js/javaScriptfile.js
+++ js/javaScriptfile.js
@@ -8,6 +8,20 @@ function toggleMenu() {
   }
 }
 
+function toggleMenu2() {
+  var box = document.getElementById('box');    
+ 
+   if(box.style.display == "block") { 
+    box.style.display = "none";
+   
+  }
+  else { // if is menuBox hidden, display it
+    box.style.display = "block";
+  
+  }
+}
+
+
 function check_pass() {
     if (document.getElementById('password').value == document.getElementById('confirm_password').value)
 		{
